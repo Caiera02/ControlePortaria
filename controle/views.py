8fa54412ae70c3b82241e0b96186224c81896c1c
Caiera02 <caiocezar2807@gmail.com>
2025-04-16 10:21:09 -0300

    add autenticação a /controle

--- controle/views.py
+++ controle/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
 from .models import Controle
 from datetime import date
 
-
+@login_required(login_url='/admin/')
 def controle_view(request):
     control = Controle.objects.all()
     buscar = request.GET.get('search')
@@ -12,7 +14,7 @@ def controle_view(request):
     else:
         hoje = date.today()
         control = control.filter(data=hoje)
-
+        
     return render(
         request,
         'controle.html',

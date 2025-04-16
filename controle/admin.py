5c4e18276a4123198c9dab912c007195294d00d6
Caiera02 <caiocezar2807@gmail.com>
2025-04-16 12:25:08 -0300

    add uma funcao que só aparece o dia atual da busca no admin

--- controle/admin.py
+++ controle/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from datetime import date
 from .models import Funcionario, Setor, Controle
 
 @admin.register(Funcionario)
@@ -15,6 +16,15 @@ class ControleAdmin(admin.ModelAdmin):
     list_display =['nome','setor','data','entrada','saida']
     list_filter = (('data',admin.DateFieldListFilter),)
     ordering= ('nome',)
+    
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+
+        # Se o usuário não estiver filtrando manualmente por data,
+        # mostra apenas os registros de hoje
+        if 'data__gte' not in request.GET and 'data__exact' not in request.GET:
+            return qs.filter(data=date.today())
+        return qs
     
     
 

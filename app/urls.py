dbf53f8caa2bda523dd5ab5f15ad305f85b329a2
Caiera02 <caiocezar2807@gmail.com>
2025-04-24 17:37:59 -0300

    add a pagina home

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from controle.views import controle_view, registra_ponto, saida_ponto
-from contas.views import login_view
+from contas.views import login_view, home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,4 +9,5 @@ urlpatterns = [
     path('register/',registra_ponto, name='register'),
     path('saida/',saida_ponto, name='saida'),
     path('login/',login_view, name='login'),
+    path('home/',home_view, name='home'),
 ]
--- contas/views.py
+++ contas/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate,login
 from django.shortcuts import render,redirect
 
@@ -11,9 +12,13 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('register')
+            return redirect('home')
         else:
             login_form = AuthenticationForm()
     else:
         login_form = AuthenticationForm()
     return render( request, 'login.html', {'login_form':login_form})
+
+@login_required(login_url='/login/')
+def home_view(request):
+    return render(request,'home.html')
